9db09772d80c09bdf8fd62e6edbba42f9e54500b
superernie77 <superernie77@gmail.com>
2017-12-01 10:02:53 +0100

    Spring Boot setup

---
+++ src/main/java/com/se77/scan/ScanRestBackendApplication.java
@@ -0,0 +1,12 @@
+package com.se77.scan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanRestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanRestBackendApplication.class, args);
+	}
+}

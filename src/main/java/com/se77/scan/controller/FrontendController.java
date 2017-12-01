67006a1d502da29934d923e3b0a24fa21ade877a
superernie77 <superernie77@gmail.com>
2017-12-01 14:19:37 +0100

    added result html page

---
+++ src/main/java/com/se77/scan/controller/FrontendController.java
@@ -0,0 +1,37 @@
+package com.se77.scan.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.se77.scan.domain.ScanMessage;
+import com.se77.scan.repo.ScanMessageRepository;
+
+/**
+ * Spring MVC-Controller class for the login and registration screen.
+ */
+@Controller
+public class FrontendController {
+
+	@Autowired
+	private ScanMessageRepository repo;
+	
+    @RequestMapping(value="/scans", method = RequestMethod.GET)
+    public ModelAndView init(){
+        ModelAndView modelAndView = new ModelAndView();
+
+        List<ScanMessage> messages = repo.findAll();
+
+        modelAndView.addObject("scans", messages);
+
+        modelAndView.setViewName("scanlist");
+
+        return modelAndView;
+    }
+
+  
+}

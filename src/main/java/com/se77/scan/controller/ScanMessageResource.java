2528f72ed2cf779b2320215dd4af28c290cbb509
superernie77 <superernie77@gmail.com>
2017-12-01 14:42:09 +0100

    added response entity for POST

--- src/main/java/com/se77/scan/controller/ScanMessageResource.java
+++ src/main/java/com/se77/scan/controller/ScanMessageResource.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.se77.scan.domain.ScanMessage;
@@ -30,10 +32,12 @@ public class ScanMessageResource {
 	private ScanMessageRepository scanMessageRepo;
 	
 	@RequestMapping(path="/message", method = RequestMethod.POST )
-	public void postMessage(@RequestBody ScanMessage message) {
+	public ResponseEntity<ScanMessage>  postMessage(@RequestBody ScanMessage message) {
 		message.setCreationTsp(Calendar.getInstance());
 		message.setId(null);
 		scanMessageRepo.save(message);
+		
+		return new ResponseEntity<ScanMessage>(message, HttpStatus.OK);
 	}
 	
 	@RequestMapping(path="/message", method = RequestMethod.GET ) 

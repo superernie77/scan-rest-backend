067411e8e2cbf3683db41b6b4f3c2a9a8b285e3d
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-11-17 10:40:48 +0100

    Integration Test added

--- src/test/java/com/se77/scan/ScanRestBackendApplicationTests.java
+++ src/test/java/com/se77/scan/ScanRestBackendApplicationTests.java
@@ -1,15 +1,52 @@
 package com.se77.scan;
 
+import org.springframework.web.client.RestClient;
+
+import com.se77.scan.domain.ScanMessage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Calendar;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment =  WebEnvironment.DEFINED_PORT)
 public class ScanRestBackendApplicationTests {
+	
+	
+	 private RestClient restClient = RestClient.create("http://localhost:8080");
+
 
 	@Test
-	public void contextLoads() {
+	public void postMessageAndRetrieveIt() {
+		
+		ScanMessage newMessage = new ScanMessage()
+				.withBarcode("12345456788")
+				.withCreatedBy("Ernie")
+				.withCreationTsp(Calendar.getInstance())
+				.withMessage("Hi There!");
+		
+		ScanMessage message = restClient.post()
+				  .uri("/message")
+				  .body(newMessage)
+				  .accept(MediaType.APPLICATION_JSON)
+				  .retrieve()
+				  .body(ScanMessage.class);
+		
+		
+		ScanMessage[] messages = restClient.get()
+				  .uri("/message")
+				  .accept(MediaType.APPLICATION_JSON)
+				  .retrieve()
+				  .body(ScanMessage[].class);
+		
+		assertTrue( messages.length == 1);
+		
+		assertEquals("12345456788", messages[0].getBarcode());
 		
 	}
 
